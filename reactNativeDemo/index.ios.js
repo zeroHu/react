8fb4663f9f8d1413a3e0dccbc577bef5109f13cc
zero <zero@zerodeMacBook-Pro.local>
2017-07-26 23:04:26 +0800

    add some

--- reactNativeDemo/index.ios.js
+++ reactNativeDemo/index.ios.js
@@ -1,53 +1,89 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
-
-export default class reactNativeDemo extends Component {
-  render() {
+import React,{ Component } from 'react'
+import{
+  AppRegistry, Image, Text, View, StyleSheet
+}
+from 'react-native'
+// Image 图片
+class Images extends Component {
+  render(){
+    let pic = {
+      uri:'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+    };
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
+      <Image source={pic} style={{marginTop:30,width:192,height:110}}></Image>
+    )
   }
 }
 
+// props
+class SayHello extends Component {
+  render(){
+    return (
+      <Text>hello {this.props.name}</Text>
+    )
+  }
+}
+
+class propsDemo extends Component {
+  render(){
+    return (
+      <View style={{marginTop:30,width:192,height:110}}>
+        <SayHello name="zero"></SayHello>
+      </View>
+    )
+  }
+}
+
+// State
+class StateFn extends Component {
+  constructor(props){
+    super()
+    this.state = {showText:false}
+    setInterval(() => {
+      this.setState(previousState => {
+        return {showText:!previousState.showText}
+      })
+    },1000)
+  }
+  render(){
+    let displaytext = this.state.showText ? this.props.name : '';
+    return (
+      <Text>this is {displaytext}</Text>
+    )
+  }
+}
+class StateDemo extends Component {
+  render(){
+    return (
+      <View style={{marginTop:30,width:192,height:110}}>
+        <StateFn name="zero"></StateFn>
+      </View>
+    )
+  }
+}
+// 样式
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+  bigblue:{
+    marginTop:50,
+    color:'blue',
+    fontWeight:'bold',
+    fontSize:30
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+  smallred:{
+    marginTop:100,
+    color:'red',
+    fontSize:12
+  }
+})
+class reactNativeDemo extends Component {
+  render(){
+    return (
+      <View>
+        <Text style={styles.bigblue}>hello world</Text>
+        <Text style={styles.smallred}>hello world</Text>
+      </View>
+    )
+  }
+}
 
 AppRegistry.registerComponent('reactNativeDemo', () => reactNativeDemo);
